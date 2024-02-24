dc2596336f02eb8686b215e34d5799f2cc450fb1
khantha <heroku.khantha2022@gmail.com>
2024-02-24 09:12:16 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const cors = require('cors')
+
+require('dotenv').config()
+const mysql = require('mysql2')
+
+const app = express()
+
+
+app.use(cors())
+
+const connection = mysql.createConnection(process.env.DATABASE_URL)
+
+app.get('/',(req, res) => {
+    console.log('Hellow World')
+    res.send('Hello word')
+})
+
+
+app.get('/attractions', (req, res) => {
+    connection.query(
+        'SELECT * FROM attractions',
+        function(err, results,fields) {
+            console.log(results)
+            res.send(results)
+        }
+    )
+})
+
+
+app.listen(process.env.PORT || 3000)
+//connection.end()
